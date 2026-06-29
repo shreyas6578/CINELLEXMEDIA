4d5147ccf9b1ed957e2d4611e411c3e915d06dbe
shreyas6758 <shreyas13.model@gmail.com>
2026-06-29 08:59:03 +0530

    views

---
+++ Controllers/ViewCounterController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ViewCounterController : Controller
+    {
+
+     public IActionResult Views()
+        {
+            
+            return View();
+        }
+    }
+}
+
--- obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b10c1b0132bcd46e4f3fac38346b36740dc881b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9b6b229e56b211cc363266ff8b19480b607f468")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
